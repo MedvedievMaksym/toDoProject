d1d79109e265bce0549e15791d58897cc564f87f
MedvedievM <medvediev.m91@gmail.com>
2020-04-25 23:10:20 +0300

    render task on page

--- src/app.js
+++ src/app.js
@@ -32,5 +32,63 @@ const tasks = [
 ];
 
 (function(arrOfTasks) {
-  
+  // конвертируем массив обьектов в обьект обьектов
+  const objOfTasks = arrOfTasks.reduce((acc, task) => {
+    acc[task._id] = task;
+    return acc;
+  }, {});
+
+  //Elements UI
+  const listContainer = document.querySelector(
+      '.tasks-list-section .list-group',
+    );
+
+  renderAllTasks(objOfTasks);
+
+  function renderAllTasks(tasksList) {
+    if(!tasksList) {
+      console.error('Передайте список задач');
+      return; 
+    }
+
+    //Создаем фрагмет будущего списка задач
+    const fragment = document.createDocumentFragment();
+    Object.values(tasksList).forEach(task => {
+      const li = listItemTemplate(task);    // создаем на каждой итерации нов.  елем.
+      fragment.appendChild(li);     //добавляем его во фрагмент 
+    });
+    listContainer.appendChild(fragment);
+
+  }
+
+  //Создает один елемент списка и возв. его
+  function listItemTemplate({ _id, title, body} = {}) {
+    const li = document.createElement('li');
+    li.classList.add(
+      'list-group-item',
+      'd-flex',
+      'align-items-center',
+      'flex-wrap',
+      'mt-2',
+    );
+    
+    const span = document.createElement('span');
+    span.textContent = title;
+    span.style.fontWeight = 'bold';
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = "Delete task";
+    deleteBtn.classList.add('btn', 'btn-danger', 'ml-auto', 'delete-btn');
+
+    const article = document.createElement('p');
+    article.textContent = body;
+    article.classList.add('mt-2', 'w-100');
+
+    li.appendChild(span);
+    li.appendChild(deleteBtn);
+    li.appendChild(article);
+    
+    return li;
+  }
+
 })(tasks);
